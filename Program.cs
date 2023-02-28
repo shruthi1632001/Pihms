0b769d3bd0389c799bd130a4e1984a16cbc9be8a
shruthi1632001 <126547387+shruthi1632001@users.noreply.github.com>
2023-02-28 11:32:37 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Hello
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello Dennis");
+            Console.Read();
+        }
+    }
+}
